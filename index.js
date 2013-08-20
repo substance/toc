e887dd86303b2f860435bbe01b1fe874f94064c4
Michael Aufreiter <michael@substance.io>
2013-08-20 22:27:22 +0200

    Sketching the new TOC view.

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var TOC = require("./toc_view");
+
+module.exports = TOC;
--- toc_view.js
+++ toc_view.js
@@ -1,20 +1,55 @@
 "use strict";
 
 var View = require("substance-application").View;
+var $$ = require("substance-application").$$;
 
 
 // Substance.TOC.View
 // ==========================================================================
 
-var TOCView = function(document) {
+var TOCView = function(doc) {
+  this.doc = doc;
   View.call(this);
 
-  this.$el.addClass('substance-toc');
+  this.$el.addClass("toc");
 };
 
 TOCView.Prototype = function() {
-  
 
+  // Handle clicks on `.heading-ref` elements in the TOC
+  // --------
+  // 
+
+  // _jumpToHeading: function(e) {
+  //   var target = $(e.currentTarget).attr('data-node');
+  //   var node = this.model.document.nodes[target];
+  //   this.selectNode(node.id);
+  //   this.jumpToNode(node);
+  //   this.updateResources();
+  //   this.updatePath();
+  //   return false;
+  // },
+
+  // Renderer
+  // --------
+
+  this.render = function() {
+
+
+    this.el.appendChild($$('a.heading-ref.level-1', {
+      text: "HELLO"
+    }));
+
+    // <% _.each(headings, function(h) { %>
+    //   <a href="#" class="heading-ref level-<%= h.level %>" data-node="<%= h.id %>"><%= h.content %></a>
+    // <% }); %>
+
+    // $(this.el).html(_.tpl('toc', {
+    //   headings: getHeadings(this.model.document)
+    // }));
+
+    return this;
+  };
 };
 
 TOCView.Prototype.prototype = View.prototype;
